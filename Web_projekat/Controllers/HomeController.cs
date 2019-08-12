14ff612fd3778323c5f90d6b5fab80dd953d2b34
stefan_dzogovic <stefandzogovicpr26@gmail.com>
2019-08-13 00:36:25 +0200

    Sesija se pravi tek kada se korisnik registruje i uloguje.

--- Web_projekat/Controllers/HomeController.cs
+++ Web_projekat/Controllers/HomeController.cs
@@ -12,22 +12,20 @@ namespace Web_projekat.Controllers
         public ActionResult Index()
         {
             Admini admini = (Admini)HttpContext.Application["admini"];
-
+            ViewBag.loggedin = false;
             User sc = (User)Session["user"];
-            if (sc == null)
-            {
-                sc = new Models.User.User();
-                Session["user"] = sc;
-            }
 
             Users users = (Users)Session["users"];
             if (users == null)
-            {
+            {   
                 users = new Users();
                 Session["users"] = users;
             }
 
-            ViewBag.user = sc;
+            if (Session["user"] != null)
+            {
+                ViewBag.user = sc;
+            }
             return View();
         }
     }

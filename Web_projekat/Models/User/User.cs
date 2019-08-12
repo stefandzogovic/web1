b30116e97b930062c0e21386817595964f010674
stefan_dzogovic <stefandzogovicpr26@gmail.com>
2019-08-13 00:41:12 +0200

    Klasa za registrovane korisnike (za sad context u global.asax, al ce se promeniti za rad sa bazom).

--- Web_projekat/Models/User/User.cs
+++ Web_projekat/Models/User/User.cs
@@ -12,6 +12,7 @@ namespace Web_projekat.Models.User
         public string password { get; set; }
         public string name { get; set; }
         public string surname { get; set; }
+        public string email { get; set; }
         public Sex sex { get; set; }
         public Role role { get; set; }
     }
---
+++ Web_projekat/Models/User/Users.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web_projekat.Models.User
+{
+    public class Users
+    {
+        public Dictionary<string, User> lista_usera = new Dictionary<string, User>();
+    }
+}

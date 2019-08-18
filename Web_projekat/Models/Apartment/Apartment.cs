a041a8d4a97c248bfa5511d50814267d6aad6c1f
stefan_dzogovic <stefandzogovicpr26@gmail.com>
2019-08-18 04:32:17 +0200

    Popravljam los commit.

---
+++ Web_projekat/Models/Apartment/Apartment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Web_projekat.Models
+{
+    public class Apartment
+    {
+
+        public int ApartmentId { get; set; }
+        public Type type { get; set; }
+        public int number_of_rooms { get; set; }
+        public int number_of_guests { get; set; }
+        // public Location location { get; set; }
+        public bool active { get; set; }
+        public double price_per_night { get; set; }
+        public List<string> comments { get; set; }
+        public List<Photo> images { get; set; }
+
+        public User.User User { get; set; }
+        public int UserId { get; set; }
+
+
+
+    }
+}

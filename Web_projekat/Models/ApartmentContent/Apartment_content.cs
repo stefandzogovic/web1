38b7398c9e74e9955c772fcd131159b2cbb7287d
stefan_dzogovic <stefandzogovicpr26@gmail.com>
2019-08-18 04:17:30 +0200

    Dodate klase za apartman i njegov sadrzaj.

---
+++ Web_projekat/Models/ApartmentContent/Apartment_content.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web_projekat.Models
+{
+    public class Apartment_content
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+
+    }
+}

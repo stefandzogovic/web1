26598aa836ee81a2ac652080300d570f3822007f
stefan_dzogovic <stefandzogovicpr26@gmail.com>
2019-08-22 04:10:52 +0200

    Sortiranje apartmana.

--- Web_projekat/Controllers/HomeController.cs
+++ Web_projekat/Controllers/HomeController.cs
@@ -12,14 +12,22 @@ namespace Web_projekat.Controllers
     {
         DataAccessLayer dal = new DataAccessLayer();
 
+        public string str;
+
         [HttpPost]
-        public ActionResult Index(string value)
+        public ActionResult Index(string selectedValue)
         {
-            return RedirectToAction("Index");
+            List<Apartment> lista = new List<Apartment>();
+
+            Session["dropdown"] = selectedValue;
+
+
+            return Json(lista);
         }
 
         public ActionResult Index()
         {
+
             Admins admini = (Admins)HttpContext.Application["admini"];
             ViewBag.loggedin = false;
             User sc = (User)Session["user"];
@@ -30,39 +38,48 @@ namespace Web_projekat.Controllers
                 ViewBag.user = sc;
             }
 
-            List<User> lista = new List<User>();
+            List<Apartment> lista = new List<Apartment>();
 
 
-            foreach (User u in dal.usersdb.ToDictionary(x => x.username, x => x).Values)
+            foreach (Apartment ap in dal.apartmentsdb.ToDictionary(x => x.ApartmentId, x => x).Values)
             {
-                User user = new User();
-                user = u;
-                user.apartments = new List<Apartment>();
+                Apartment apartment = new Apartment();
+                apartment = ap;
+                apartment.images = new List<Photo>();
 
-                foreach (Apartment ap in dal.apartmentsdb.ToDictionary(x => x.ApartmentId, x => x).Values)
+                foreach (Photo ph in dal.photosdb.ToDictionary(x => x.PhotoId, x => x).Values)
                 {
-                    Apartment apartment = new Apartment();
-                    apartment = ap;
-                    apartment.images = new List<Photo>();
-
-                    foreach (Photo ph in dal.photosdb.ToDictionary(x => x.PhotoId, x => x).Values)
+                    Photo photo = new Photo();
+                    photo = ph;
+                    if (apartment.ApartmentId == photo.ApartmentId)
                     {
-                        Photo photo = new Photo();
-                        photo = ph;
-                        if(apartment.ApartmentId == photo.ApartmentId)
-                        {
-                            apartment.images.Add(photo);
-                        }
+                        apartment.images.Add(photo);
                     }
-
-
                 }
 
-                lista.Add(user);
+                lista.Add(apartment);
+
+
             }
 
-            ViewBag.lista = lista;
+
+            if ((string)Session["dropdown"] == "sortasc")
+            {
+                ViewBag.lista = lista.OrderBy(x => x.price_per_night).ToList();
+            }
+            else if ((string)Session["dropdown"] == "sortdesc")
+            {
+                ViewBag.lista = lista.OrderByDescending(x => x.price_per_night).ToList();
+
+            }
+            else
+            {
+                ViewBag.lista = lista;
+
+            }
+
             return View();
         }
+
     }
 }

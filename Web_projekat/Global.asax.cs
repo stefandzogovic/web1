6faabad66c53527a526514e0d9b9ce77457c5ac7
stefan_dzogovic <stefandzogovicpr26@gmail.com>
2019-09-05 16:22:38 +0200

    Ako je prazna baza dodaj available amenities.

--- Web_projekat/Global.asax.cs
+++ Web_projekat/Global.asax.cs
@@ -19,7 +19,38 @@ namespace Web_projekat
 
             DataAccessLayer dal = new DataAccessLayer();
             dal.Database.CreateIfNotExists();
-           
+
+            List<string> basic = new List<string> { "Wifi", "Laptop_Friendly_Workspace", "Cable_TV", "Washer", "Air_Conditioning", "TV", "Heating" };
+            List<string> family = new List<string> { "Crib", "High_Chair", "Travel_Crib", "Room-darkening_Shades", "Window_Guards" };
+            List<string> facility = new List<string> { "Elevator", "Paid_Parking_Off_Premices", "Single_Level_Home_(No_Stairs)", "Free_Street_Parking" };
+            List<string> dining = new List<string> { "Kitchen", "Coffee_Maker", "Cooking_Basics(Pots,_Pans,_Salt_Pepper", "Dishes_and_Silverware", "Microwave", "Refrigerator" };
+
+            if (dal.availableamenitiesdb.ToList().Count == 0)
+            {
+                 foreach(string str in basic)
+                 {
+                     dal.availableamenitiesdb.Add(new Models.AvailableAmenities { type = 1, name = str.Replace('_', ' ')});
+                 }
+                 foreach (string str in family)
+                 {
+
+                     dal.availableamenitiesdb.Add(new Models.AvailableAmenities { type = 2, name = str.Replace('_', ' ') });
+                 }
+                 foreach (string str in facility)
+                 {
+
+                         dal.availableamenitiesdb.Add(new Models.AvailableAmenities { type = 3, name = str.Replace('_', ' ') });
+                 }
+                 foreach (string str in dining)
+                 {
+
+                         dal.availableamenitiesdb.Add(new Models.AvailableAmenities { type = 4, name = str.Replace('_', ' ') });
+                 }
+
+
+                 dal.SaveChanges();
+                 
+            }
 
             Admins admini = new Admins("~/App_Data/admini.txt");
             HttpContext.Current.Application["admini"] = admini;
